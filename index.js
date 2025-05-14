513c6682a7fe5f395a2acaddfcb4339c2018a0eb
Adamu Abubakar <engr.adamulab@gmail.com>
2025-05-14 10:57:19 +0100

    added logic for parsing payloads

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all request witha string
 const server = http.createServer(function (req, res) {
@@ -24,11 +25,22 @@ const server = http.createServer(function (req, res) {
   //   Get the headers as an object
   const headers = req.headers;
 
-  // Send the response
-  res.end("Hello wolrd\n");
+  //   Get the payloads, if there is any
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", function (data) {
+    buffer += decoder.write(data);
+  });
+  req.on("end", function () {
+    buffer += decoder.end();
 
-  // Log the request path
-  console.log("Request received with these headers", headers);
+    //
+    // Send the response
+    res.end("Hello wolrd\n");
+
+    // Log the request path
+    console.log("Request received with this payload: ", buffer);
+  });
 });
 
 // Start the server, have it listen on port 5000
